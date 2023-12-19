beffc0d36246b7154186b6a0d969bc8bb3d5a00d
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-12-19 12:38:09 +0200

    applied locomotive css and js

---
+++ script.js
@@ -0,0 +1,12 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(details){
+        document.querySelector("#miniCircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
