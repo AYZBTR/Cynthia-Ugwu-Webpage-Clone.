91d227428f2d472ea95a71c47001d36502299bd8
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-12-19 13:01:31 +0200

    Applied gsap on headings and subheadings

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y:'-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+    .to(".boundingelem", {
+        y:'0',
+        duration: 2,
+        ease: Expo.easeInOut,
+        delay:-1,
+        stagger:.2
+    })
+
+    .from("#herofooter", {
+        y:-10,
+        opacity: 0,
+        duration: 1.5,
+        delay:-.1,
+        ease: Expo.easeInOut
+    })
+}
+
 function circleMouseFollower(){
     window.addEventListener("mousemove", function(details){
         document.querySelector("#miniCircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
@@ -10,3 +37,4 @@ function circleMouseFollower(){
 }
 
 circleMouseFollower();
+firstPageAnim();
